b8250c598d18a15b24f00d1e137e0bd38530072e
RishaanVats <mishra00730@gmail.com>
2025-12-07 15:40:26 +0530

    Reworked Skills Component

--- src/app/components/resume/resume.ts
+++ src/app/components/resume/resume.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-resume',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './resume.html',
   styleUrls: ['./resume.css'],
 })
