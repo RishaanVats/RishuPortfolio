e22af9470898dcbe61e06d02676b9fe5c52373d4
RishaanVats <mishra00730@gmail.com>
2025-12-10 16:08:44 +0530

    Case Studies page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,11 +5,13 @@ import { SkillsComponent } from './components/skills/skills';
 import { ProjectsComponent } from './components/projects/projects';
 import { ResumeComponent } from './components/resume/resume';
 import { ContactComponent} from './components/contact/contact';
+import { CaseStudiesComponent } from './components/case-studies/case-studies'
 
 export const routes: Routes = [
   { path: '', component: HeroComponent },
   { path: 'about', component: AboutComponent },
   { path: 'skills', component: SkillsComponent },
+  { path: 'case-studies', component: CaseStudiesComponent },
   { path: 'projects', component: ProjectsComponent },
   { path: 'resume', component: ResumeComponent },
   { path: 'contact', component: ContactComponent },
