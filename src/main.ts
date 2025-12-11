b4120be8cb30ec6e6409147ede104cb2d2bc830c
RishaanVats <mishra00730@gmail.com>
2025-12-11 12:42:55 +0530

    Case Studeis page with clcik to rach an element on the page.

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,16 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
 import { routes } from './app/app.routes';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 
 bootstrapApplication(App, {
   ...appConfig,
-  providers: [...(appConfig.providers || []), provideRouter(routes)],
+  providers: [...(appConfig.providers || []), provideRouter(routes,
+    withInMemoryScrolling({
+        scrollPositionRestoration: 'enabled',
+        anchorScrolling: 'enabled', // <-- This enables the scrolling to a particular element using id
+      })
+  )],
 })
   .catch((err) => console.error(err));
